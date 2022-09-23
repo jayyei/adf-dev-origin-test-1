36800f86fee41608f7389979f6f70f0174236d4a
Jay Sanchez <47990218+jayyei@users.noreply.github.com>
2022-09-23 02:59:23 -0500

    Update delete-ls.js

--- build/delete-ls.js
+++ build/delete-ls.js
@@ -5,7 +5,7 @@
   // })
   
 var fs = require('fs')
-var template = require('build/armTemplate/ARMTemplateForFactory.json')
+var template = require('./armTemplate/ARMTemplateForFactory.json')
 
 console.log('Reading ARM Template')
 
@@ -20,7 +20,7 @@ template.resources = templateWithoutServices
 console.log("Overriding template")
 var jsonToWrite = JSON.stringify(template, null, 2)
 
-fs.writeFile('build/armTemplate/ARMTemplateForFactoryNoLSV1Node.json', jsonToWrite, (err) => {
+fs.writeFile('./armTemplate/ARMTemplateForFactoryNoLSV1Node.json', jsonToWrite, (err) => {
   if (err) throw err
   console.log('Linked Services were succesfully removed')
 })
