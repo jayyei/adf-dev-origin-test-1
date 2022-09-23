cdbd672fbb23387a791abdb47c91f24540ea8b57
Jay Sanchez <47990218+jayyei@users.noreply.github.com>
2022-09-23 03:06:29 -0500

    Update delete-ls.js

--- build/delete-ls.js
+++ build/delete-ls.js
@@ -20,7 +20,7 @@ template.resources = templateWithoutServices
 console.log("Overriding template")
 var jsonToWrite = JSON.stringify(template, null, 2)
 
-fs.writeFile('./armTemplate/ARMTemplateForFactoryNoLSV1Node.json', jsonToWrite, (err) => {
+fs.writeFile('./armTemplate/ARMTemplateForFactory.json', jsonToWrite, (err) => {
   if (err) throw err
   console.log('Linked Services were succesfully removed')
 })
