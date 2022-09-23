c87b2734e8c2429d0a2b97c7acd5aee731ccc19d
Jay Sanchez <47990218+jayyei@users.noreply.github.com>
2022-09-23 02:48:26 -0500

    Create delete-ls.js

---
+++ build/delete-ls.js
@@ -0,0 +1,27 @@
+// fs.readFile('ArmTemplateForFactory.json', (err, data) => {
+  //   if (err) throw err
+  //   let template = JSON.parse(data)
+  //   console.log(template)
+  // })
+  
+var fs = require('fs')
+var template = require('build/armTemplate/ARMTemplateForFactory.json')
+
+console.log('Reading ARM Template')
+
+var templateWithoutServices = template.resources.filter(
+  resource => resource.type !== "Microsoft.DataFactory/factories/linkedServices" 
+)
+
+console.log("Removing linked services")
+template.resources = templateWithoutServices
+
+
+console.log("Overriding template")
+var jsonToWrite = JSON.stringify(template, null, 2)
+
+fs.writeFile('./ARMTemplateForFactoryNoLSV1Node.json', jsonToWrite, (err) => {
+  if (err) throw err
+  console.log('Linked Services were succesfully removed')
+})
+
